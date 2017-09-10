bfd68a0afaa5415f965172abee9769521d92b585
yetzt <git@yetzt.me>
2017-09-10 14:09:33 +0200

    add maximum queue length

--- dbnc.js
+++ dbnc.js
@@ -1,13 +1,19 @@
 #!/usr/bin/env node
 
-function dbnc(t){
+function dbnc(t,n){
 	return (this instanceof dbnc) ? (function(self,t){
 		self.t = t;
+		self.n = n || null;
 		self.x = null;
 		self.c = 0;
 		return function(fn) {
 			if (self.x) clearTimeout(self.x);
 			self.c++;
+			if (self.n && self.c >= self.n) return (function(){
+				fn(self.c);
+				self.x = null;
+				self.c = 0;
+			})();
 			self.x = setTimeout(function(){
 				fn(self.c);
 				self.x = null;
--- readme.md
+++ readme.md
@@ -2,6 +2,13 @@
 
 a very simple debounce function.
 
+## constructor
+
+`debunce = new dbnc(wait, max)`
+
+wait is the time to wait for new calls till execution
+max is the maximum number of call after which execution is forced
+
 ## example
 
 ```javascript
